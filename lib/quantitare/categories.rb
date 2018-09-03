863861596af6e7610d994bb95272c4f1feb04cca
Tyler Guillen <tyguillen@gmail.com>
2018-09-03 15:22:34 -0700

    init

---
+++ lib/quantitare/categories.rb
@@ -0,0 +1,7 @@
+require "quantitare/categories/version"
+
+module Quantitare
+  module Categories
+    # Your code goes here...
+  end
+end
---
+++ lib/quantitare/categories/version.rb
@@ -0,0 +1,5 @@
+module Quantitare
+  module Categories
+    VERSION = "0.1.0"
+  end
+end
