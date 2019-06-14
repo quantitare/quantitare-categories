b29144a17b91de4469e735e9740a986735e7203f
Tyler Guillen <tyguillen@gmail.com>
2019-06-13 20:53:59 -0700

    MVP for categories

--- lib/quantitare/categories.rb
+++ lib/quantitare/categories.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
+require 'dry/schema'
+require 'active_support/inflector'
+
 require 'quantitare/categories/version'
+require 'quantitare/category'
+require 'quantitare/category/name_parser'
+
+Dir[File.expand_path('categories/*.rb', __dir__)].each { |file| require file }
 
 module Quantitare
   ##
   # = Quantitare Categories
   #
   module Categories
-    class << self
-
-    end
   end
 end
--- lib/quantitare/category.rb
+++ lib/quantitare/category.rb
@@ -1,7 +1,97 @@
 # frozen_string_literal: true
 
 module Quantitare
+  ##
+  # Root class for a Quantitare category. Provides the validation logic and metadata for a scrobble's +data+ attribute.
+  # The specification DSL uses the +dry-schema+ gem under the hood.
+  #
+  #   class Quantitare::Categories::Music < Quantitare::Category
+  #     specification do
+  #
+  #     end
+  #   end
+  #
   class Category
-    include Virtus::Model
+    SPECIFICATIONS_IVAR_NAME = '@specifications'
+    ROOT_CATEGORY = self
+
+    class << self
+      attr_accessor :specification_blk
+      attr_reader :name
+
+      def inherited(subklass)
+        subklass.name = NameParser.new(subklass).name
+      end
+
+      def name=(new_name)
+        specifications.delete(name)
+        @name = new_name
+        specifications[name] = self
+      end
+
+      def [](name)
+        specifications[name]
+      end
+
+      def get(category_name)
+        ROOT_CATEGORY[category_name] || default
+      end
+
+      def schema
+        @schema ||= Dry::Schema.JSON(&specification_blk)
+      end
+
+      def specification(&blk)
+        self.specification_blk = blk
+      end
+
+      def default
+        ROOT_CATEGORY
+      end
+
+      def icon
+        nil
+      end
+
+      private
+
+      def specifications
+        return specifications_from_root if specifications_from_root
+
+        ROOT_CATEGORY.instance_variable_set(SPECIFICATIONS_IVAR_NAME, {})
+
+        specifications_from_root
+      end
+
+      def specifications_from_root
+        ROOT_CATEGORY.instance_variable_get(SPECIFICATIONS_IVAR_NAME)
+      end
+    end
+
+    self.name = 'default'
+
+    attr_reader :data
+
+    def initialize(data)
+      @data = data
+    end
+
+    def icon
+      self.class.icon
+    end
+
+    def name
+      self.class.name
+    end
+
+    def valid?
+      schema.(data).errors.empty?
+    end
+
+    private
+
+    def schema
+      self.class.schema
+    end
   end
 end
